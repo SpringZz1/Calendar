9a29ebd5f046fc41fba61744fd7816ad39612b34
SpringZz <wang90944@gmail.com>
2022-09-08 19:50:21 +0800

    添加公共样式和方法模块

---
+++ src/components/Calendar/store.js
@@ -0,0 +1,30 @@
+import { update } from './date/render';
+export function reactive ({ year, month })  {
+  const dateInfo = {};
+  // const _dateInfo = getDateInfo();
+  const _dateInfo = [year, month];
+
+  Object.defineProperties(dateInfo, {
+    year: {
+      get() {
+        return _dateInfo[0];
+      },
+      set(newValue) {
+        _dateInfo[0] = newValue;
+        update(_dateInfo[0], _dateInfo[1]);
+      },
+    },
+    month: {
+      get() {
+        return _dateInfo[1];
+      },
+      set(newValue) {
+        _dateInfo[1] = newValue;
+        // update(_dateInfo[0], 1);
+        update(_dateInfo[0], _dateInfo[1]);
+      },
+    },
+  });
+
+  return dateInfo;
+};
